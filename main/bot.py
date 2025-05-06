c2a126ec9fa1d1552dc8908cf5b7cd9e13ae5a8d
Floated <F104T3D@proton.me>
2025-05-07 00:36:45 +1000

    Fixed various issues in repository and bot.py. Added bot join date command

--- main/bot.py
+++ main/bot.py
@@ -32,7 +32,7 @@ async def on_message(message):
     await bot.process_commands(message)
 
 @bot.command()
-async def member(ctx, user: str):
+async def member_join(ctx, user: str):
     if ctx.guild:
         for member in ctx.guild.members:
             if member.name == user:
